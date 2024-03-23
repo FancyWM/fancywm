92df2b3feac771ae2b48073c315cadfdf1cbcc1d
Vesko Karaganev <vesko.karaganev@gmail.com>
2024-03-23 15:01:24 +0000

    Add Package project for the Microsoft Store
    microsoft_store.yml builds .msixbundles ready for upload to the
    Microsoft Store.

--- FancyWM/AssemblyInfo.cs
+++ FancyWM/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FancyWM")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,8 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.11.0.0")]
-[assembly: AssemblyFileVersion("2.11.0.0")]
 // Expose internals to tests
 [assembly: InternalsVisibleTo("FancyWM.Tests")]
 // Wrap thrown non-exception type objects in RuntimeWrappedException in all handlers in this assembly.
