648e9018e03d45e5e2505e7ca6e5c28da7ffbf46
Vesko Karaganev <vesko.karaganev@gmail.com>
2026-01-11 09:42:32 +0000

    Hide VDS tooltip if explorer supports it

---
+++ FancyWM/Utilities/ExplorerFeature.cs
@@ -0,0 +1,21 @@
+﻿using FancyWM.DllImports;
+
+namespace FancyWM.Utilities
+{
+    internal static class ExplorerFeature
+    {
+        public static bool HasVirtualDesktopTooltip()
+        {
+            HWND it = new();
+            while ((it = PInvoke.FindWindowEx(new(), it, "XamlExplorerHostIslandWindow", null)) != default)
+            {
+                WINDOWS_EX_STYLE exStyle = unchecked((WINDOWS_EX_STYLE)PInvoke.GetWindowLong(it, GetWindowLongPtr_nIndex.GWL_EXSTYLE));
+                if (exStyle.HasFlag(WINDOWS_EX_STYLE.WS_EX_TOPMOST) && exStyle.HasFlag(WINDOWS_EX_STYLE.WS_EX_NOACTIVATE))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
